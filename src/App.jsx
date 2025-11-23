ab9b03ea8c5d4617507837a81ae3dfc65bac7c32
xxxVisheshxxx <visheshshrivastavadmo@gmail.com>
2025-11-24 01:09:31 +0530

    removed useless route nesting

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,18 @@ import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <Routes>
-      <Route
-        path="/*"
-        element={
-          <MainLayout>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/courses" element={<Courses />} />
-              <Route path="/courses/new" element={<CourseForm />} />
-              <Route path="/courses/:id" element={<CourseDetail />} />
-              <Route path="/courses/:id/edit" element={<CourseForm />} />
-              <Route path="/attendance" element={<Attendance />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/profile" element={<Profile />} />
-            </Routes>
-          </MainLayout>
-        }
-      />
-    </Routes>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/new" element={<CourseForm />} />
+        <Route path="/courses/:id" element={<CourseDetail />} />
+        <Route path="/courses/:id/edit" element={<CourseForm />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </MainLayout>
   );
 }
